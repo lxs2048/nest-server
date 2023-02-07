8627f5b9e2363ebff21469f3f14b954774567647
zhangsan <lxs2048@sina.com>
2023-02-07 19:41:52 +0800

    获取个人信息与优化上传头像

--- src/mini/mini-user/mini-user.controller.ts
+++ src/mini/mini-user/mini-user.controller.ts
@@ -8,6 +8,7 @@ import {
   Delete,
   UseInterceptors,
   UploadedFile,
+  Request,
 } from '@nestjs/common';
 import { MiniUserService } from './mini-user.service';
 import { CreateMiniUserDto } from './dto/create-mini-user.dto';
@@ -18,29 +19,46 @@ import { bucketTopDir } from 'src/common/enums/common.enum';
 import { AllowAnon } from 'src/common/decorators/allow-anon.decorator';
 import { Roles } from 'src/common/decorators/roles.decorator';
 import { Role } from 'src/common/enums/common.enum';
+import { extname } from 'path';
+import { ConfigService } from '@nestjs/config';
 @Controller('user')
 export class MiniUserController {
   constructor(
     private readonly miniUserService: MiniUserService,
     private readonly ossService: OssService,
+    private readonly config: ConfigService,
   ) {}
 
+  /* 登录 */
   @Post('login')
   @AllowAnon()
   login(@Body() createMiniUserDto: CreateMiniUserDto) {
     return this.miniUserService.login(createMiniUserDto);
   }
 
+  /* 上传头像 */
   @Post('upload')
   @AllowAnon()
   @UseInterceptors(FileInterceptor('file'))
   async upload(@UploadedFile() file) {
-    const { originalname, buffer } = file;
+    const { buffer } = file;
+    const fileName = `${
+      new Date().getTime() + Math.random().toString(36).slice(-7) + extname(file.originalname)
+    }`;
     const ret = await this.ossService.putBuffer(
-      `${bucketTopDir.AvatarImg}/${originalname}`,
+      `${bucketTopDir.AvatarImg}/${fileName}`,
       buffer,
     );
-    return ret ? ret.url : '';
+    const website = this.config.get('ali.website')
+    console.log(ret);
+    return ret ? `${website}/${ret.name}` : '';
+  }
+
+  /* 获取个人信息 */
+  @Get('selfInfo')
+  getselfInfo(@Request() req) {
+    // 该方法需要登录验证，所以可以使用jwt验证查询到的用户信息
+    return req.user || {};
   }
 
   @Get()
