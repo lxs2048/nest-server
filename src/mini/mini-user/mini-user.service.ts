4d2a615fda489490cd7eda6e64ef3094477a3e42
zhangsan <lxs2048@sina.com>
2023-03-29 15:15:44 +0800

    获取注册或登录的用户信息

--- src/mini/mini-user/mini-user.service.ts
+++ src/mini/mini-user/mini-user.service.ts
@@ -1,12 +1,74 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { InjectRepository } from '@nestjs/typeorm';
+import { RedisUtilService } from 'src/common/libs/redis/redis.service';
+import { customException } from 'src/common/utils/customException';
+import { Repository } from 'typeorm';
+import axios from 'axios';
 import { CreateMiniUserDto } from './dto/create-mini-user.dto';
 import { UpdateMiniUserDto } from './dto/update-mini-user.dto';
+import { MiniUserEntity } from './entities/mini-user.entity';
 
 @Injectable()
 export class MiniUserService {
-  login(createMiniUserDto: CreateMiniUserDto) {
-    console.log(createMiniUserDto);
-    return 'This action adds a new miniUser';
+  constructor(
+    @InjectRepository(MiniUserEntity)
+    private readonly miniUserRepository: Repository<MiniUserEntity>,
+    private readonly config: ConfigService,
+    private readonly redisService: RedisUtilService,
+  ) {}
+  async login(createMiniUserDto: CreateMiniUserDto) {
+    const { loginCode, phoneCode } = createMiniUserDto;
+    const phoneNumber = await this.getPhone(phoneCode);
+    // 查看手机号是否存在
+    const user = await this.findBy({ phone: phoneNumber });
+    let _user = null;
+    if (!user) {
+      // 创建用户
+      _user = {};
+      // 保存用户信息
+      try {
+        // 1. 获取openid:
+        const loginData = await axios.get(
+          `https://api.weixin.qq.com/sns/jscode2session?appid=${this.config.get(
+            'xcx.AppID',
+          )}&secret=${this.config.get(
+            'xcx.AppSecret',
+          )}&js_code=${loginCode}&grant_type=authorization_code`,
+        );
+        const loginInfo = loginData['data'] || {};
+        if (!loginInfo.openid) customException.fail('客户端登录code无效', 400);
+        const ret = await this.miniUserRepository.save({
+          phone: phoneNumber,
+          openid: loginInfo.openid,
+        });
+        _user = ret;
+      } catch (_) {
+        customException.fail('创建用户信息失败', 500);
+      }
+    } else {
+      _user = user;
+    }
+    // 处理登录
+    console.log(_user);
+    /*
+    {
+      // id: 'f5548296-b4d0-4049-b4b9-aa8fdf22e84b',
+      phone: '(86)17835204878',
+      openid: 'oGVaj5B2BI35RARDhDBc8dZhvqrA',
+      nickname: null,
+      avatarurl: null,
+      id: 'f5548296-b4d0-4049-b4b9-aa8fdf22e84b',
+      createdAt: 2023-03-29T07:06:45.609Z,
+      updatedAt: 2023-03-29T07:06:45.609Z,
+      role: 1,
+      status: 1
+    }
+    */
+    return {
+      token: '',
+      userInfo: {},
+    };
   }
 
   findAll() {
@@ -23,5 +85,34 @@ export class MiniUserService {
 
   remove(id: number) {
     return `This action removes a #${id} miniUser`;
+  }
+  async findBy(data: any) {
+    return this.miniUserRepository.findOneBy(data);
+  }
+  /* 获取手机号 */
+  async getPhone(phoneCode: string) {
+    // 校验code
+    if (!phoneCode) {
+      customException.fail('缺少获取手机号参数code');
+    }
+    // 获取token
+    const AccesstToken = await this.redisService.get('access_token_wx_xcx');
+    if (!AccesstToken) {
+      customException.fail('服务端微信token不存在', 500);
+    }
+    // 获取手机信息 https://developers.weixin.qq.com/miniprogram/dev/OpenApiDoc/user-info/phone-number/getPhoneNumber.html
+    try {
+      const phoneData = await axios.post(
+        `https://api.weixin.qq.com/wxa/business/getuserphonenumber?access_token=${AccesstToken}`,
+        {
+          code: phoneCode,
+        },
+      );
+      const phoneInfo = phoneData['data']['phone_info'];
+      const { phoneNumber, countryCode } = phoneInfo;
+      return `(${countryCode})${phoneNumber}`;
+    } catch (error) {
+      customException.fail('获取手机号失败', 500);
+    }
   }
 }
